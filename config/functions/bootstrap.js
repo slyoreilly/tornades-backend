ee53644ae64cfe962cd882ac87ac31c3776a2f3f
Sylvain O'Reilly <soreilly@syncstats.com>
2021-04-18 10:00:24 -0400

    superAdmin

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,43 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+//module.exports = () => {};
+module.exports = async () => {
+
+      const params = {
+        username: process.env.DEV_USER || 'Sylvain',
+        password: process.env.DEV_PASS || 'isaac3!',
+        firstname: process.env.DEV_USER || 'Sylvain',
+        lastname: process.env.DEV_USER || "O'Reilly",
+        email: process.env.DEV_EMAIL || 'soreilly@syncstats.com',
+        blocked: false,
+        isActive: true,
+      };
+      //Check if any account exists.
+      const admins = await strapi.query('user', 'admin').find();
+  
+      if (admins.length === 0) {
+       try {
+          let tempPass = params.password;
+          let verifyRole = await strapi.query('role', 'admin').findOne({ code: 'strapi-super-admin' });
+          if (!verifyRole) {
+          verifyRole = await strapi.query('role', 'admin').create({
+            name: 'Super Admin',
+            code: 'strapi-super-admin',
+            description: 'Super Admins can access and manage all features and settings.',
+           });
+          }
+          params.roles = [verifyRole.id];
+          params.password = await strapi.admin.services.auth.hashPassword(params.password);
+          await strapi.query('user', 'admin').create({
+            ...params,
+          });
+          strapi.log.info('Admin account was successfully created.');
+          strapi.log.info(`Email: ${params.email}`);
+          strapi.log.info(`Password: ${tempPass}`);
+        } catch (error) {
+          strapi.log.error(`Couldn't create Admin account during bootstrap: `, error);
+        }
+      
+    }
+  };
